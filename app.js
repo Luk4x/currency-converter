b13049545db9fce5d05e8ce8d617c0f42435c59a
Luk4x <luk4xm4ci3l@gmail.com>
2022-03-15 05:46:53 -0300

    getting data from api

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
+// request function
+function get(url) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send();
+    return request.responseText;
+}
+
+// currency API
+const request = currency => {
+    let url = `https://free.currconv.com/api/v7/convert?q=${currency}&compact=ultra&apiKey=6b6c6149f67139c54d38`;
+    const api = get(url);
+    const apiJ = JSON.parse(api);
+    return apiJ[currency];
+};
+
+// 2° Select.
 const option = document.getElementById('to-coins');
+// foreign country flag image
 const convertedCurrencyImage = document.getElementById('converted-image');
+// currency name and value in real (under the flag image)
 const convertedCurrencyName = document.querySelector('.converted-symbol-name');
+// currency symbol and value converted
 const convertedValue = document.querySelector('.converted-value');
 let currencySymbol = `&#8364;`;
-let quotation = 5.54;
+// initial select quotation (euro)
+let quotation = request('EUR_BRL');
+convertedCurrencyName.textContent = `Euro (R$ ${quotation.toFixed(3).replace('.', ',')})`;
+// input value on content
+const userValue = document.getElementById('user-value');
+// input value on result
+const toConvertValue = document.querySelector('.to-convert-value');
+// button
+const convertButton = document.querySelector('.convert-button');
 
+// changing elements according to the selected option
 option.addEventListener('change', () => {
     let newSrc = '';
     let currencyName = '';
@@ -12,37 +41,35 @@ option.addEventListener('change', () => {
         newSrc = './assets/europe-flag.ico';
         currencyName = 'Euro';
         currencySymbol = `&#8364;`;
-        quotation = 5.54;
+        quotation = request('EUR_BRL');
     } else if (option.selectedIndex === 1) {
         newSrc = './assets/us-flag.svg';
         currencyName = 'Dollar';
         currencySymbol = `&#36;`;
-        quotation = 5.08;
+        quotation = request('USD_BRL');
     } else if (option.selectedIndex === 2) {
-        newSrc = './assets/bitcoin-symbol.svg';
-        currencyName = 'Bitcoin';
-        currencySymbol = `&#8383;`;
-        quotation = 198721.78;
-    } else if (option.selectedIndex === 3) {
         newSrc = './assets/japan-flag.svg';
         currencyName = 'Yen';
         currencySymbol = '&#165;';
-        quotation = 0.043;
+        quotation = request('BRL_JPY');
     }
     convertedCurrencyImage.src = newSrc;
     convertedCurrencyName.textContent = `${currencyName} (R$ ${quotation.toFixed(3).replace('.', ',')})`;
     convertedValue.innerHTML = `${currencySymbol} 0`;
 });
 
-// onchange input, change real and reset other.
-
+// converting action
 const convert = () => {
-    const userValue = document.getElementById('user-value');
-    const toConvertValue = document.querySelector('.to-convert-value');
-    toConvertValue.textContent = `R$ ${parseFloat(userValue.value).toFixed(2).replace('.', ',')}`;
-    let result = parseFloat(userValue.value) * quotation;
-    convertedValue.innerHTML = `${currencySymbol} ${result.toFixed(2)}`;
+    if (userValue.value == '') {
+        userValue.style.borderColor = 'red';
+        toConvertValue.textContent = 'R$ 0,00';
+        convertedValue.innerHTML = `${currencySymbol} 0.00`;
+    } else {
+        userValue.style.borderColor = '#3b3c47';
+        result = parseFloat(userValue.value) * quotation;
+        toConvertValue.textContent = `R$ ${parseFloat(userValue.value).toFixed(2).replace('.', ',')}`;
+        convertedValue.innerHTML = `${currencySymbol} ${result.toFixed(2)}`;
+    }
 };
 
-const convertButton = document.querySelector('.convert-button');
 convertButton.addEventListener('click', convert);
